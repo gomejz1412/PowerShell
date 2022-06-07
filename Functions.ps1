cf8bc28abd3525bad9822ad9dfef4c64e02d6759
gomejz1412 <106976195+gomejz1412@users.noreply.github.com>
2022-06-06 17:23:17 -0700

    Add files via upload

---
+++ Functions.ps1
@@ -0,0 +1,23 @@
+﻿$computername 
+$now = Get-Date
+$uptime = $now - $boot | select -ExpandProperty totalhours | foreach {$_.ToString("###.##")}
+
+$os = Get-WmiObject win32_operatingsystem -Property caption,lastbootuptime -ComputerName $computername
+$cdrive = Get-WmiObject win32_logicaldisk -Filter "deviceID='c:'" -ComputerName $computername
+$freespace = $cdrive.FreeSpace /1GB -as [INT]
+$boot = $os.ConvertToDateTime($os.LastBootUpTime)
+
+
+
+
+$properties = [ordered]@{'computername' = $computername;
+                         'OS' = $os.caption;
+                         'Lastbootup' = $boot;
+                         'uptimehours' = $uptime;
+                         'C:Freespace' = $freespace
+                         }    
+$obj = New-Object -TypeName PSObject -Property $properties
+
+
+$FormatEnumerationLimit = -1
+Write-Output $obj
