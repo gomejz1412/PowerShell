dff49219e78a18631b83f7298e810221d95dbb82
gomejz1412 <106976195+gomejz1412@users.noreply.github.com>
2022-06-06 20:40:46 -0700

    Add files via upload

---
+++ Password gen.ps1
@@ -0,0 +1,21 @@
+﻿clear-host
+set-location C:\Pass
+
+$passwordlength = 88                
+$uppercaseletters = (65..90)
+$lowercaseletters = (97..122)
+$numberszerothroughnine = (48..57)
+$specialcharacters = (33..47)
+$morespecialcharacters = (58..64)
+$curlybrackets = (123..126)
+
+
+
+$a = -join ($uppercaseletters +
+           $lowercaseletters +
+           $numberszerothroughnine +
+           $morespecialcharacters +
+           $curlybrackets |
+Get-Random -Count $passwordlength | % {[char]$_})
+$a
+$a | clip
